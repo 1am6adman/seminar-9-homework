c41f6db66efa8d94f4a090a70377c0058ba1ad67
1am6adman <iam6adman@outlook.com>
2023-05-28 11:47:28 +0300

    add example 066

---
+++ Example066/Program.cs
@@ -0,0 +1,17 @@
+﻿int Prompt(string message)
+{
+    Console.Write(message);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+int SumElem(int n, int m)
+{
+    if (n == m) return n;
+    else return SumElem(n + 1, m) + n;
+}
+
+int m = Prompt("Введите M: ");
+int n = Prompt("Введите N: ");
+
+Console.WriteLine(SumElem(m, n));
