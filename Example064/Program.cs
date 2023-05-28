4ed5997fb358447ee242451598674df19a884e6b
1am6adman <iam6adman@outlook.com>
2023-05-28 11:20:14 +0300

    add example 064

---
+++ Example064/Program.cs
@@ -0,0 +1,21 @@
+﻿int Prompt(string message)
+{
+    Console.Write(message);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+int NaturalNum(int n, int m)
+{
+    if (n == m) return n;
+    else Console.Write($"{NaturalNum(n, m + 1)}, ");
+    return m;
+}
+
+int n = Prompt("Введите N: ");
+if (n < 1)
+{
+    Console.WriteLine("Ввдите положительное число");
+    return;
+}
+Console.WriteLine(NaturalNum(n, 1));
